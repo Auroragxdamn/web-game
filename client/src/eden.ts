7b1381fa06f2ea0b7e5ec022b195a33889cea974
Emre <emre.kosen4@gmail.com>
2026-03-16 16:55:39 +0200

    deleted client

--- client/src/eden.ts
+++
@@ -1,5 +0,0 @@
-import { treaty } from "@elysiajs/eden";
-import type { App } from "../server/src/index";
-
-// Point the treaty client to our Elysia server running on port 3000
-export const client = treaty<App>("http://localhost:3000");

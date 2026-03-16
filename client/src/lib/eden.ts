a84eb9fcaa5d393d84fc4865e1e8534f4eb5c545
Emre <emre.kosen4@gmail.com>
2026-03-16 22:56:52 +0200

    bugfix 29

--- client/src/lib/eden.ts
+++ client/src/lib/eden.ts
@@ -1,6 +1,4 @@
 import { treaty } from '@elysiajs/eden'
-// Import the type from your root server file (adjust the relative path if needed)
-import type { App } from '../../../server/src/index.ts'
+import type { App } from '@server/index'
 
-// Initialize Eden with your server's URL
 export const api = treaty<App>('http://localhost:3000')
